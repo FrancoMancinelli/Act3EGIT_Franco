b3aadc66c2f550e4da6407ecf9f0eaa26f02da1b
Franco <franco.e.mancinelli@gmail.com>
2021-11-25 20:21:52 +0100

    Añado el gitignore y añado el metodo java solicitado

---
+++ src/paquete/Metodos.java
@@ -0,0 +1,32 @@
+package paquete;
+
+public class Metodos {
+
+	public static void main(String[] args) {
+		
+		int[] array = new int [20];
+		valoresRandomArray(array, 1, 10);
+		
+	}
+
+	/**
+	 * Devuelve un array con numeros aleatorios segun un intervalo de numeros
+	 * seleccionado
+	 * @param m Array a la cual se le agregaran los valores random
+	 * @param x Valor MINIMO del que pueden partir los numeros aleatorios
+	 * @param y Valor MAXIMO al que pueden llegar los numeros aleatorios
+	 * @return
+	 */
+	public static int[] valoresRandomArray(int[] a, int x, int y) {
+
+		//Agrega valores numericos random a un array
+		for (int i = 0; i < a.length; i++) {
+				a[i] = (int) (Math.random() * (y + 1 - x)) + x;
+			}
+	
+		return a;
+	}
+	
+
+
+}
